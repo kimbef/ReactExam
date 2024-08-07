ca54764112701b3227fc0c3b1dd07f45b9c4b9e8
kimbeff <kimbeff@gmail.com>
2024-08-07 18:36:38 +0300

    .

--- my-blog/client/src/common/gen.js
+++
@@ -1,8 +0,0 @@
-export const serverUrl = "http://localhost:3030/jsonstore";
-export const serverEndpoints = {
-  register: "/users/authors",
-  login: "/users/authors",
-  logout: "/users/authors/logout",
-  usersDetails: "/users/me",
-  createComment: "/users/authors",
-};
